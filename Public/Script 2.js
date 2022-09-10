7f98a5b3f3272ea23650db50d6ac6aba10f15393
BowenMichael <34144478+BowenMichael@users.noreply.github.com>
2022-09-10 14:14:28 -0400

    My Snap Project

---
+++ Public/Script 2.js
@@ -0,0 +1,6 @@
+// -----JS CODE-----
+// @input SceneObject myObj
+
+var pos = script.myObj.getTransform().getWorldPosition();
+
+script.myObj.setTransform(1,1,1)
---
+++ Public/Script.js
@@ -0,0 +1,10 @@
+// -----JS CODE-----
+// @input float speed
+// @input Asset.AnimationLayer
+// @input function
+// @input SceneObject myObj
+// @input transf
+
+print("hello world")
+
+script.myObj.enabled = !script.myObj.enabled;
